e229ab43c93120507e7117d505b50c1e18b0428d
Cory Lawrence <35942282+clawrence199@users.noreply.github.com>
2020-07-23 20:54:00 -0500

    Initial Commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 8, -10);
+    private Vector3 offsetRotation = new Vector3(20, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.SetPositionAndRotation(player.transform.position + offset, Quaternion.Euler(offsetRotation));
+    }
+}
